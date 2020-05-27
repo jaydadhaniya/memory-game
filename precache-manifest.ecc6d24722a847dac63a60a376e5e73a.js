daebd5b9909d785ea10d9f3368d33586bc3cfbc8
Jay Dadhaniya <jaydadhaniya45@gmail.com>
2020-05-27 09:22:35 +0530

    deploy build project

---
+++ precache-manifest.ecc6d24722a847dac63a60a376e5e73a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a66550cac49dbd0b581",
+    "url": "/memory-game/css/app.34ca30ae.css"
+  },
+  {
+    "revision": "91f3c06e30065a1d2579",
+    "url": "/memory-game/css/chunk-vendors.658bae4e.css"
+  },
+  {
+    "revision": "e517be186447bd3ce9d925331ccbe89f",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "7a66550cac49dbd0b581",
+    "url": "/memory-game/js/app.41680f54.js"
+  },
+  {
+    "revision": "91f3c06e30065a1d2579",
+    "url": "/memory-game/js/chunk-vendors.3407d8b4.js"
+  },
+  {
+    "revision": "8dc341de3428049269eec4dcdd9257f4",
+    "url": "/memory-game/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/memory-game/robots.txt"
+  }
+]);
